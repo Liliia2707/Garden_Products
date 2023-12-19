6ad2b89da4faadce372e942168583d3aa6a6c35d
Liliia Pavlova <pavloval4449@gmail.com>
2023-12-18 22:33:20 -0800

    done

--- src/Components/DataForm/DataForm.jsx
+++ src/Components/DataForm/DataForm.jsx
@@ -7,7 +7,7 @@ export const DataForm = () => {
         <input type="text" placeholder="Name" />
         <input type="text" placeholder="Phone number" />
         <input type="text" placeholder="Email" />
-        <input type="submit" value="Get a discount" />
+        <input type="submit" value="Get a discount" id={styles.submit_button} />
       </form>
     </div>
   );
