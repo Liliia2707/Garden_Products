cd02736240c903a76f61e7a9ecd4f7eec5e9c0ae
Liliia Pavlova <pavloval4449@gmail.com>
2024-01-30 14:25:17 -0800

    button link single page

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { Outlet } from "react-router-dom";
 import "./App.css";
-import { Footer } from "./Layaut/Footer/Footer";
-import { Header } from "./Layaut/Header/Header";
+import { Footer } from "./Layout/Footer/Footer";
+import { Header } from "./Layout/Header/Header";
 import { Context } from "./Providers/Context";
 
 function App() {
